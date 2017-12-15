c3c3abe7c16b5f8286ecd93d80a8042ba42ceb51
Leo <leo.m@hotmail.co.uk>
2017-12-15 01:00:22 +0000

    Create app

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+// default...
+@import "~bootstrap/scss/bootstrap.scss";
+// custom bootstrap...
+// @import "~bootstrap/scss/_nav.scss";
+// custom app...
+#app {
+
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <b-button href="#" variant="success">I am a Link</b-button>
+    </div>
+    <template v-for="(tenant, n) in tenants">
+      <ul>
+        <li :tenant="tenant">
+          {{ tenant.firstName }}
+          {{ tenant.lastName }}
+        </li>
+      </ul>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome!',
+      tenants: [
+        {
+          firstName: "Tom",
+          lastName: "Jones"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
